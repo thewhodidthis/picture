f41dad044eea6ad056397a4b99368d27b6862f89
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-09-07 19:30:21 +0200

    rework

--- example/index.js
+++ example/index.js
@@ -1,54 +1,68 @@
 'use strict';
 
-var Picture = window.picture;
-
 var html = document.documentElement;
-var canvas = document.getElementById('canvas');
-var filter = document.getElementById('filter');
+var master = document.getElementById('canvas');
 var sprite = document.createElement('img');
 
 var w = 800;
 var h = 600;
+var x = (master.width - w) * 0.5;
+var y = (master.height - h) * 0.5;
+var count = 0;
+
+var draw = function() {
+  picture.source(sprite).target(master, x, y);
+};
+
+var pimp = function() {
+  picture.filter().target(master, x, y);
+}
 
 var picture = new Picture(w, h);
 
-var diff = {
-  x: (canvas.width - w) * 0.5,
-  y: (canvas.height - h) * 0.5
-};
+picture.filter = function() {
+  var w = this.context.canvas.width;
+  var h = this.context.canvas.height;
+
+  var output = this.context.getImageData(0, 0, w, h);
+  var outputData = output.data;
+
+  for (var i = 0, stop = outputData.length; i < stop; i += 4) {
+    var r = outputData[i];
+    var g = outputData[i+1];
+    var b = outputData[i+2];
+    var a = outputData[i+3];
+
+    outputData[i+2] = outputData[i];
+    outputData[i+3] = (outputData[i+2] == 255) ? 0 : a;
+  }
+
+  this.context.putImageData(output, 0, 0);
+
+  return this;
+}
 
 html.className = 'html';
 
 if (window !== window.top) {
-  html.classList.add('is-iframe');
+  html.className = html.className + ' is-iframe';
 }
+
+sprite.addEventListener('load', function _onLoad(e) {
+  draw();
+}, false);
 
 sprite.src = '/sprite.jpg';
 
-sprite.addEventListener('load', function _onLoad(e) {
-  picture.source(sprite).target(canvas, diff.x, diff.y);
+document.addEventListener('click', function _onClick(e) {
+  e.preventDefault();
+  e.stopPropagation();
 
-  picture.filter = function() {
-    var w = this.context.canvas.width;
-    var h = this.context.canvas.height;
-
-    var output = this.context.getImageData(0, 0, w, h);
-    var outputData = output.data;
-
-    for (var i = 0, stop = outputData.length; i < stop; i += 4) {
-      var r = outputData[i];
-      var g = outputData[i+1];
-      var b = outputData[i+2];
-      var a = outputData[i+3];
-
-      outputData[i+2] = outputData[i];
-      outputData[i+3] = (outputData[i+2] == 255) ? 0 : a;
-    }
-
-    this.context.putImageData(output, 0, 0);
-
-    return this;
+  if (count % 2 === 0) {
+    pimp();
+  } else {
+    draw();
   }
 
-  picture.filter().target(filter, diff.x, diff.y);
-}, false);
+  count += 1;
+}, false)

8b4e270cf7a8f8ed4b46657914ad7fb97f026e66
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-09-07 19:31:41 +0200

    rebuild

--- dist/picture.js
+++ dist/picture.js
@@ -1,11 +1,12 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.picture = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Picture = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-function Picture(w, h) {
+function Picture(width, height) {
+  this.options = Object.create(Picture.defaults);
   this.context = document.createElement('canvas').getContext('2d');
 
-  this.context.canvas.width = w || Picture.defaults.w;
-  this.context.canvas.height = h || Picture.defaults.h;
+  this.context.canvas.width = width || this.options.width;
+  this.context.canvas.height = height || this.options.height;
 }
 
 Picture.prototype = {
@@ -46,8 +47,8 @@ Picture.prototype = {
 };
 
 Picture.defaults = {
-  w: 500,
-  h: 300
+  width: 500,
+  height: 300
 };
 
 module.exports = Picture;

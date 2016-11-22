c5bca93e85f2b52286889da0d4891ca69b781981
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-11-22 19:43:13 +0200

    rebuild using rollup, babel

--- index.js
+++ index.js
@@ -4,46 +4,48 @@ function Picture(width, height) {
   this.options = Object.create(Picture.defaults);
   this.context = document.createElement('canvas').getContext('2d');
 
-  this.context.canvas.width = width || this.options.width;
-  this.context.canvas.height = height || this.options.height;
+  this.context.canvas.width = parseInt(width, 10) || this.options.width;
+  this.context.canvas.height = parseInt(height, 10) || this.options.height;
 }
 
 Picture.prototype = {
   constructor: Picture,
 
-  _copy: function(source, target, sx, sy, dx, dy, w, h) {
-    if (source instanceof Picture) {
-      source = source.context.canvas;
+  copy: function copy(s, d, sX, sY, dX, dY, w, h) {
+    var source = s;
+    var target = d;
+
+    // s: source, d: destination
+    if (s instanceof Picture) {
+      source = s.context.canvas;
     }
 
-    if (target instanceof Picture) {
-      target = target.context.canvas;
+    if (d instanceof Picture) {
+      target = d.context.canvas;
     }
 
-    var sx = sx || 0;
-    var sy = sy || 0;
-    var dx = dx || 0;
-    var dy = dy || 0;
+    var sx = sX || 0;
+    var sy = sY || 0;
+    var dx = dX || 0;
+    var dy = dY || 0;
 
-    var w = w || source.width;
-    var h = h || source.height;
+    var width = w || source.width;
+    var height = h || source.height;
 
-    w = w - sx;
-    h = h - sy;
+    width -= sx;
+    height -= sy;
 
-    target.getContext('2d').drawImage(source, sx, sy, w, h, dx, dy, w, h);
+    target.getContext('2d').drawImage(source, sx, sy, width, height, dx, dy, width, height);
 
     return this;
   },
-
-  source: function(target, x, y) {
-    this._copy(target, this, x, y, 0, 0);
+  source: function source(canvas, x, y) {
+    this.copy(canvas, this, x, y, 0, 0);
 
     return this;
   },
-
-  target: function(target, x, y) {
-    this._copy(this, target, 0, 0, x, y);
+  target: function target(canvas, x, y) {
+    this.copy(this, canvas, 0, 0, x, y);
 
     return this;
   }

e5b37dfb48a4350fe7c3696437ab6e407c72347b
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-19 22:05:04 +0200

    Cleanup example

--- example/index.js
+++ example/index.js
@@ -10,43 +10,47 @@ var x = (canvas.width - w) * 0.5;
 var y = (canvas.height - h) * 0.5;
 var count = 0;
 
+var keyout = {
+  filter: function() {
+    var w = this.width;
+    var h = this.height;
+
+    var output = this.context.getImageData(0, 0, w, h);
+    var outputData = output.data;
+
+    for (var i = 0, stop = outputData.length; i < stop; i += 4) {
+      var r = outputData[i];
+      var g = outputData[i+1];
+      var b = outputData[i+2];
+      var a = outputData[i+3];
+
+      outputData[i+2] = outputData[i];
+      outputData[i+3] = (outputData[i+2] == 255) ? 0 : a;
+    }
+
+    this.context.putImageData(output, 0, 0);
+
+    return this;
+  }
+};
+
+var master = Picture(w, h);
+var output = Object.assign(master, keyout);
+
 var draw = function() {
-  picture.source(sprite).target(canvas, x, y);
+  master.source(sprite).target(canvas, x, y);
 };
 
 var pimp = function() {
-  picture.filter().target(canvas, x, y);
+  output.filter().target(canvas, x, y);
 }
-
-var picture = new Picture(w, h);
-
-picture.filter = function() {
-  var w = this.context.canvas.width;
-  var h = this.context.canvas.height;
-
-  var output = this.context.getImageData(0, 0, w, h);
-  var outputData = output.data;
-
-  for (var i = 0, stop = outputData.length; i < stop; i += 4) {
-    var r = outputData[i];
-    var g = outputData[i+1];
-    var b = outputData[i+2];
-    var a = outputData[i+3];
-
-    outputData[i+2] = outputData[i];
-    outputData[i+3] = (outputData[i+2] == 255) ? 0 : a;
-  }
-
-  this.context.putImageData(output, 0, 0);
-
-  return this;
-}
-
-html.className = 'html';
 
 if (window !== window.top) {
   html.className = html.className + ' is-iframe';
 }
+
+sprite.addEventListener('load', draw, false);
+sprite.setAttribute('src', 'sprite.jpg');
 
 canvas.addEventListener('click', function _onClick(e) {
   e.preventDefault();
@@ -59,7 +63,4 @@ canvas.addEventListener('click', function _onClick(e) {
 
   count += 1;
 }, false)
-
-sprite.addEventListener('load', draw, false);
-sprite.setAttribute('src', 'sprite.jpg');
 

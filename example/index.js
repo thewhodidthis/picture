8b7d24ac1285176b7f3289d99621946f76cac495
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-04-01 13:13:04 +0300

    Update example

--- example/index.js
+++ example/index.js
@@ -5,9 +5,9 @@ const TAU = Math.PI * 2;
 const Loop = (callback) => {
   let frameId;
 
-  const play = (fn) => window.requestAnimationFrame(fn);
+  const play = fn => window.requestAnimationFrame(fn);
   const stop = () => window.cancelAnimationFrame(frameId);
-  const loop = t => {
+  const loop = () => {
     callback(frameId);
 
     if (frameId) {
@@ -21,12 +21,12 @@ const Loop = (callback) => {
   };
 };
 
-// https://en.wikipedia.org/wiki/Regular_polygon
 const Poly = (size, n = 5) => {
   const center = size * 0.5;
 
   // So I can use with `Array.map()` straight away
   const points = Array.from({ length: n }).map((point, i) => {
+    // https://en.wikipedia.org/wiki/Regular_polygon
     const a = (i * TAU) / n;
     const x = center * Math.cos(a);
     const y = center * Math.sin(a);
@@ -37,90 +37,65 @@ const Poly = (size, n = 5) => {
   return points;
 };
 
-const Star = (size, n, m = 2) => {
-  // This is only accurate for the sizes I'm interested in right here
-  const isDegenerate = n % m === 0;
-
-  const points = Poly(size, n);
-  const output = Array.from({ length: n * m }).map((v, i) => {
-    const j = i * m;
-    const k = isDegenerate ? (j + j % m) / m : 0;
-    const x = (k + j) % n;
-
-    return points[x];
-  });
-
-  return output;
-};
-
 const Node = (size, r) => {
   const picture = Picture(size);
   const context = picture.context;
   const connect = p => context.lineTo(p.x, p.y);
 
   const center = size * 0.5;
-
   const render = (details, i) => {
-    context.rotate(r * (i + 1));
+    context.rotate(r);
     context.beginPath();
 
+    // The points
     details.forEach(connect);
 
     context.closePath();
-    context.stroke();
+
+    context.fillStyle = i % 2 ? '#fff' : '#000';
+    context.fill();
   };
 
-  return Object.assign({
-    render(details, i) {
-      context.strokeStyle = '#fff';
-
+  const output = {
+    render(details) {
       context.save();
       context.translate(center, center);
 
+      // The shapes
       details.forEach(render);
 
       context.restore();
 
       return this;
     },
-  }, picture);
+  };
+
+  return Object.assign(picture, output);
 };
 
 const canvas = document.getElementById('canvas');
 const [w, h] = [canvas.width, canvas.height];
 const master = Picture(w, h);
-const center = {
-  x: w * 0.5,
-  y: h * 0.5,
-};
 
-const getSize = (size, n) => size * Math.cos(Math.PI / n);
+const size = 160;
+const grid = w / size;
+const seed = [5, 4, 7, 8, 3, 6];
+const getR = (i, s = 90, p = 5) => s - ((p * i) + i);
 
-// Corners
-const sizes = [150, 150, 150, 150];
-const polys = [7, 5, 4, 3].map((n, i) => Poly(getSize(90, n) - i * 10, n));
+const layers = Array.from({ length: 18 });
+const shapes = seed.map((n, j) => layers.map((v, i) => Poly(getR(i), n)));
+const update = Loop((frame) => {
+  const r = 0.01 * frame;
 
-// Core
-const stars = [9, 5].map((n, i) => Star(getSize(180 - i * 20, n), n));
+  // Tiles
+  shapes.map((shape, i) => Node(size, i % 2 ? r + (i * 0.5) : -r)).forEach((node, i) => {
+    const p = shapes[i];
+    const x = (i % grid);
+    const y = (i - x) / grid;
 
-const draw = (frame) => {
-  const r = 0.0125 * frame;
-
-  const roses = sizes.map(size => Node(size, r));
-
-  roses.forEach((rose, i) => {
-    const s = rose.canvas.width;
-    const l = (i % 2) * (w - s);
-    const t = Math.floor(i * 0.5) * (h - s);
-
-    rose.render(polys).target(master, l, t);
+    node.render(p).target(master, size * x, size * y);
   });
-
-  const [x, y] = [center.x - (180 * 0.5), center.y - (180 * 0.5)];
-  const focus = Node(180, r);
-
-  focus.render(stars).target(master, x, y);
-};
+});
 
 canvas.parentNode.replaceChild(master.canvas, canvas);
 
@@ -128,8 +103,6 @@ if (window !== window.top) {
   document.documentElement.className = 'is-iframe';
 }
 
-const loop = Loop(draw);
-
-document.addEventListener('click', loop);
-window.addEventListener('load', loop);
+document.addEventListener('click', update);
+window.addEventListener('load', update);
 

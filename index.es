b742ff74c7ea0eb48af1ca7bbd0588380af7ac2f
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-10 00:22:12 +0200

    module: fix for eslint sans semicolons, rebuild

--- index.es
+++ index.es
@@ -2,54 +2,53 @@
 // Super minimal canvas helpers
 
 // `CanvasRenderingContext2D.drawImage` wrapper
-export function picture(s, d, sX, sY, dX, dY) {
+export const picture = (s, d, sX, sY, dX, dY) => {
   // Decide whether source/target objects are canvas elements or
   // context-like by checking for the canvas property
-  const source = s.canvas || s;
-  const target = d.canvas || d;
+  const source = s.canvas || s
+  const target = d.canvas || d
 
   // Avoid default params for now
-  const sx = sX || 0;
-  const sy = sY || 0;
-  const dx = dX || 0;
-  const dy = dY || 0;
+  const sx = sX || 0
+  const sy = sY || 0
+  const dx = dX || 0
+  const dy = dY || 0
 
   // Apparently no transpiler penalties over here
-  const [w, h] = [source.width - sx, source.height - sy];
+  const [w, h] = [source.width - sx, source.height - sy]
 
   // Choose destination
-  const context = target.context || target.getContext('2d');
+  const context = target.context || target.getContext('2d')
 
   // Wipe out
-  context.clearRect(dx, dy, w, h);
+  context.clearRect(dx, dy, w, h)
 
   // And draw
-  context.drawImage(source, sx, sy, w, h, dx, dy, w, h);
+  context.drawImage(source, sx, sy, w, h, dx, dy, w, h)
 }
 
 // No type cheching of course
 export const from = canvas => ({
-  get context() {
-    return this.canvas.getContext('2d');
+  get context () {
+    return this.canvas.getContext('2d')
   },
   canvas,
-  source(copy, x, y) {
-    picture(copy, this.context, x, y);
+  source (copy, x, y) {
+    picture(copy, this.context, x, y)
 
-    return this;
+    return this
   },
-  target(copy, x, y) {
-    picture(this.context, copy, 0, 0, x, y);
+  target (copy, x, y) {
+    picture(this.context, copy, 0, 0, x, y)
 
-    return this;
-  },
-});
+    return this
+  }
+})
 
 export const createPicture = (w, h) => {
   // Create and resize offscreen `canvas`, square up if height missing
-  const sample = document.createElement('canvas');
-  const canvas = Object.assign(sample, { width: w, height: h || w });
+  const canvas = document.createElement('canvas')
+  const sample = Object.assign(canvas, { width: w, height: h || w })
 
-  return from(canvas);
-};
-
+  return from(sample)
+}
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 // Super minimal canvas helpers
 
 // `CanvasRenderingContext2D.drawImage` wrapper
-function picture(s, d, sX, sY, dX, dY) {
+var picture = function (s, d, sX, sY, dX, dY) {
   // Decide whether source/target objects are canvas elements or
   // context-like by checking for the canvas property
   var source = s.canvas || s;
@@ -19,11 +19,11 @@ function picture(s, d, sX, sY, dX, dY) {
   var dy = dY || 0;
 
   // Apparently no transpiler penalties over here
-  var w = source.width - sx,
-      h = source.height - sy;
+  var ref = [source.width - sx, source.height - sy];
+  var w = ref[0];
+  var h = ref[1];
 
   // Choose destination
-
   var context = target.context || target.getContext('2d');
 
   // Wipe out
@@ -31,36 +31,35 @@ function picture(s, d, sX, sY, dX, dY) {
 
   // And draw
   context.drawImage(source, sx, sy, w, h, dx, dy, w, h);
-}
-
-// No type cheching of course
-var from = function from(canvas) {
-  return {
-    get context() {
-      return this.canvas.getContext('2d');
-    },
-    canvas: canvas,
-    source: function source(copy, x, y) {
-      picture(copy, this.context, x, y);
-
-      return this;
-    },
-    target: function target(copy, x, y) {
-      picture(this.context, copy, 0, 0, x, y);
-
-      return this;
-    }
-  };
 };
 
-var createPicture = function createPicture(w, h) {
-  // Create and resize offscreen `canvas`, square up if height missing
-  var sample = document.createElement('canvas');
-  var canvas = Object.assign(sample, { width: w, height: h || w });
+// No type cheching of course
+var from = function (canvas) { return ({
+  get context () {
+    return this.canvas.getContext('2d')
+  },
+  canvas: canvas,
+  source: function source (copy, x, y) {
+    picture(copy, this.context, x, y);
 
-  return from(canvas);
+    return this
+  },
+  target: function target (copy, x, y) {
+    picture(this.context, copy, 0, 0, x, y);
+
+    return this
+  }
+}); };
+
+var createPicture = function (w, h) {
+  // Create and resize offscreen `canvas`, square up if height missing
+  var canvas = document.createElement('canvas');
+  var sample = Object.assign(canvas, { width: w, height: h || w });
+
+  return from(sample)
 };
 
 exports.picture = picture;
 exports.from = from;
 exports.createPicture = createPicture;
+

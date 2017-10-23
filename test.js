fe2aee8cda7344d346aca1534f69952df5e0a3c9
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-23 08:39:08 +0200

    test: cleanup

--- test.js
+++ test.js
@@ -1,29 +1,32 @@
 import 'cutaway'
-import { stat, veto } from 'tapeless'
+import { report, assert } from 'tapeless'
 import { from as pictureFrom, createPicture } from './index.es'
 
-const is = veto((a, b) => a === b, 'equals')
-const isNot = veto((a, b) => a !== b, 'equals not')
+const { ok, equal, notEqual } = assert
 
 const canvas = document.createElement('canvas')
 
-const p1 = createPicture(20)
-const p2 = createPicture()
+const p1 = createPicture()
+const p2 = createPicture(1)
 
-is(p1.canvas.height, 20, 'is equal', 'will default')
-is(p2.canvas.height, 0, 'is equal')
+ok(p1, null, 'will default')
+ok(p2)
+
+equal(p1.canvas.height, 0, 'is equal')
+equal(p2.canvas.height, 1, 'is equal')
 
 const p3 = pictureFrom(canvas)
+const { width, height } = p3.canvas
 
-is(p3.canvas.width + p3.canvas.height, 450, 'will transfer dimensions')
+equal(width + height, 450, 'will transfer dimensions')
 
 const p4 = pictureFrom(p2.canvas)
 
-is(p2.context, p4.context, 'is equal', 'will adjust context')
+equal(p2.context, p4.context, 'is equal', 'will adjust context')
 
 p4.canvas = canvas
 
-isNot(p2.context, p4.context, 'is not equal')
-is(p4.context, canvas.getContext('2d'), 'is equal')
+notEqual(p2.context, p4.context, 'is not equal')
+equal(p4.context, canvas.getContext('2d'), 'is equal')
 
-stat()
+report()

dff225509e8b5c7d11dcc1cc5b5c36e01fbfce0a
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-15 14:11:14 +0100

    example: rework using bare es6 modules

--- example/index.js
+++ example/index.js
@@ -1,107 +1,44 @@
-(function () {
-'use strict';
+import { createPicture, from } from './picture.js'
 
-// # Picture
-// Canvas drawing helpers
+const canvas = document.querySelector('canvas')
+const target = from(canvas)
 
-// `CanvasRenderingContext2D.drawImage` wrapper
-var picture = function (s, d, sX, sY, dX, dY) {
-  // Decide whether source/target objects are canvas elements or
-  // context-like by checking for the canvas property
-  var source = s.canvas || s;
-  var target = d.canvas || d;
+const { width: w, height: h } = canvas
 
-  // Avoid default params for now
-  var sx = sX || 0;
-  var sy = sY || 0;
-  var dx = dX || 0;
-  var dy = dY || 0;
+const source = document.createElement('img')
+const buffer = createPicture(w, h)
 
-  // Apparently no transpiler penalties over here
-  var ref = [source.width - sx, source.height - sy];
-  var w = ref[0];
-  var h = ref[1];
+let frame = 0
+let shift = 0
 
-  // Choose destination
-  var context = target.context || target.getContext('2d');
+const period = 15
+const frames = 40 * w
 
-  // Wipe out
-  context.clearRect(dx, dy, w, h);
-
-  // And draw
-  context.drawImage(source, sx, sy, w, h, dx, dy, w, h);
-};
-
-var from = function (canvas) { return ({
-  get context() {
-    return this.canvas.getContext('2d')
-  },
-  canvas: canvas,
-  source: function source(copy, x, y) {
-    picture(copy, this.context, x, y);
-
-    return this
-  },
-  target: function target(copy, x, y) {
-    picture(this.context, copy, 0, 0, x, y);
-
-    return this
-  }
-}); };
-
-var createPicture = function (width, height) {
-  if ( height === void 0 ) height = width;
-
-  // Setup and resize offscreen `canvas`
-  var canvas = document.createElement('canvas');
-  var sample = Object.assign(canvas, { width: width, height: height });
-
-  return from(sample)
-};
-
-var canvas = document.querySelector('canvas');
-var target = from(canvas);
-
-var w = canvas.width;
-var h = canvas.height;
-
-var source = document.createElement('img');
-var buffer = createPicture(w, h);
-
-var frame = 0;
-var shift = 0;
-
-var period = 15;
-var frames = 40 * w;
-
-var draw = function () {
+const draw = () => {
   if (frame % period === 0) {
-    buffer.source(source, shift, 0).target(target);
+    buffer.source(source, shift, 0).target(target)
 
-    shift += w;
-    shift %= frames;
+    shift += w
+    shift %= frames
   }
 
-  frame = window.requestAnimationFrame(draw);
-};
-
-var html = document.documentElement;
-
-if (window !== window.top) {
-  html.classList.add('is-iframe');
+  frame = window.requestAnimationFrame(draw)
 }
 
-html.classList.add('is-mining');
+const html = document.documentElement
 
-source.addEventListener('load', function () {
-  html.classList.remove('is-mining');
+if (window !== window.top) {
+  html.classList.add('is-iframe')
+}
 
-  draw();
-});
+html.classList.add('is-mining')
+
+source.addEventListener('load', () => {
+  html.classList.remove('is-mining')
+
+  draw()
+})
 
 // Source video from
 // https://www.pond5.com/stock-footage/44599211
-source.setAttribute('src', 'source.gif');
-
-}());
-
+source.setAttribute('src', 'clip.gif')

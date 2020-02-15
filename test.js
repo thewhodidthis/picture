4d59c302cc769612132872428521e4b948acecdd
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-02-15 14:22:14 +0000

    test: fix for tapeless latest

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import 'cutaway'
-import { report, assert } from 'tapeless'
+import { report, assert } from 'tapeless/browser'
 import { from as pictureFrom, createPicture } from './index.mjs'
 
 const { ok, equal, notEqual } = assert
@@ -9,24 +9,38 @@ const canvas = document.createElement('canvas')
 const p1 = createPicture()
 const p2 = createPicture(1)
 
-ok(p1, null, 'will default')
-ok(p2)
+ok
+  .test(p1)
+  .describe(null, 'will default')
+  .test(p2)
 
-equal(p1.canvas.height, 0, 'is equal')
-equal(p2.canvas.height, 1, 'is equal')
+equal
+  .describe('is equal')
+  .test(p1.canvas.height, 0)
+  .describe('is equal')
+  .test(p2.canvas.height, 1)
 
 const p3 = pictureFrom(canvas)
 const { width, height } = p3.canvas
 
-equal(width + height, 450, 'will transfer dimensions')
+equal
+  .describe('will transfer dimensions')
+  .test(width + height, 450)
 
 const p4 = pictureFrom(p2.canvas)
 
-equal(p2.context, p4.context, 'is equal', 'will adjust context')
+equal
+  .describe('is equal')
+  .test(p2.context, p4.context)
 
 p4.canvas = canvas
 
-notEqual(p2.context, p4.context, 'is not equal')
-equal(p4.context, canvas.getContext('2d'), 'is equal')
+notEqual
+  .describe('is not equal')
+  .test(p2.context, p4.context)
+
+equal
+  .describe('is equal', 'will adjust context')
+  .test(p4.context, canvas.getContext('2d'))
 
 report()

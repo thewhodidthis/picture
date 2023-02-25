cad81b8bc92ce8c84a584c8fe7d102779f904630
thewhodidthis <thewhodidthis@fastmail.com>
2023-02-25 21:18:40 +0000

    git: ZZKKVVV erznxr

---
+++ main.js
@@ -0,0 +1,53 @@
+// # Picture
+// Canvas drawing helpers
+
+// A thin wrapper to mimic `CanvasRenderingContext2D.drawImage`.
+export function picture(s, d, sX, sY, dX, dY) {
+  // Decide whether source/target objects are canvas elements or
+  // context-like by checking for the canvas property.
+  const source = s.canvas || s
+  const target = d.canvas || d
+
+  // Avoid default params for now.
+  const sx = sX || 0
+  const sy = sY || 0
+  const dx = dX || 0
+  const dy = dY || 0
+
+  // Apparently no transpiler penalties over here.
+  const [w, h] = [source.width - sx, source.height - sy]
+
+  // Choose destination.
+  const context = target.context || target.getContext("2d")
+
+  // Wipe out.
+  context.clearRect(dx, dy, w, h)
+
+  // And draw.
+  context.drawImage(source, sx, sy, w, h, dx, dy, w, h)
+}
+
+export const from = (canvas, options) => ({
+  get context() {
+    return this.canvas.getContext("2d", options)
+  },
+  canvas,
+  source(copy, x, y) {
+    picture(copy, this.context, x, y)
+
+    return this
+  },
+  target(copy, x, y) {
+    picture(this.context, copy, 0, 0, x, y)
+
+    return this
+  },
+})
+
+export function createPicture(width, height = width, options) {
+  // Set up and resize offscreen `canvas`.
+  const canvas = document.createElement("canvas")
+  const sample = Object.assign(canvas, { width, height })
+
+  return from(sample, options)
+}
